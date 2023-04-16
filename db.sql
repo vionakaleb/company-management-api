e51494d77bca7812727a0f7c8d6a3dfb64737576
Viona Kaleb <vionakaleb@gmail.com>
2023-04-17 06:22:22 +0700

    update data type sql

--- db.sql
+++ db.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE company_management;
 
-CREATE TABLE company (id serial PRIMARY KEY, name varchar(255) NOT NULL, address varchar(255) NOT NULL, revenue integer NOT NULL, phone_code integer NOT NULL, phone_number integer NOT NULL);
+CREATE TABLE company (id serial PRIMARY KEY, name varchar(255) NOT NULL, address varchar(255) NOT NULL, revenue bigint NOT NULL, phone_code bigint NOT NULL, phone_number bigint NOT NULL);
 CREATE TABLE office (id serial PRIMARY KEY, name varchar(255) NOT NULL, location_latitude varchar(255) NOT NULL, location_longitude varchar(255) NOT NULL, office_start_date varchar(255) NOT NULL, company_id integer NOT NULL, FOREIGN KEY (company_id) REFERENCES company (id));
 
 INSERT INTO company (name, address, revenue, phone_code, phone_number) VALUES ('Test Company', 'Jakarta Selatan', 100000000000, 62, 82112345);
